7a50330b23866c645a020e208c1240e8eb9cdab5
Thalles Gabriel <thallesgabrieldasilvacosta@gmail.com>
2025-08-07 13:48:23 -0300

    Agora vai

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
+from typing import List, Dict, Any
 from app.cafe import Cafe
-from app.errors import (
-    NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
-)
-from typing import Dict, Any
+from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
 
-
-def go_to_cafe(visitor: Dict[str, Any], cafe: Cafe) -> None:
-    try:
-        message = cafe.visit_cafe(visitor)
-        print(message)
-    except (
-            NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
-    ) as error:
-        print(error)
+def go_to_cafe(friends: List[Dict[str, Any]], cafe: Cafe) -> str:
+    result = []
+    for friend in friends:
+        try:
+            message = cafe.visit_cafe(friend)
+            result.append(message)
+        except NotVaccinatedError:
+            result.append(f"{friend['name']} is not vaccinated.")
+        except OutdatedVaccineError:
+            result.append(f"{friend['name']}'s vaccine is outdated.")
+        except NotWearingMaskError:
+            result.append(f"{friend['name']} is not wearing a mask.")
+    return "\n".join(result)
